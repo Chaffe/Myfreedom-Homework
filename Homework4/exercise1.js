cd88996efb2b643a04d4ea648aebb30d9e63ff86
Chaffe <mudrogelov.beled@gmail.com>
2020-07-30 13:20:37 +0300

    feat: add exercise 1 solution

---
+++ Homework4/exercise1.js
@@ -0,0 +1,11 @@
+let tagName = "p";
+let color = "green";
+let tagContent = "Lorem ipsum dolor, sit amet consectetur adipisicing."
+
+addTagColorContent(tagName, color, tagContent);
+
+function addTagColorContent(tagName, color, tagContent) {
+  let tag = document.createElement(tagName);
+  tag.style.backgroundColor = color;
+  tag.textContent = tagContent;
+}
