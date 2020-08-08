9ef49724ab9ec72776f5293d67679a4a745245fe
Chaffe <mudrogelov.beled@gmail.com>
2020-08-08 22:48:21 +0300

    feat: add exercise1 solution

---
+++ Homework5/exercise1.js
@@ -0,0 +1,47 @@
+/*1. На странице в html задать список элементов li с числами.
+Джававскриптом нужно выбрать из этого списка четные числа и создать из них массив.
+Затем, используя метод forEach, создать из массива новый спсок элементов li и поместить эти элементы в исходный тег ul.
+Для обхода через querySelectorAll использовать цикл for-of (forEach работать не будет)*/
+
+addNumbersElements();
+
+function addNumbersElements () {
+  let ul = document.querySelector('ul');
+  let numbers = [];
+  let evenNumbers = [];
+
+  for (let i = 1; i <= 10; i++) {
+    let li = document.createElement('li');
+    li.textContent = i;
+    ul.appendChild(li);
+  
+    numbers.push(i);
+  }
+
+  addEvenNumbersElements(numbers);
+}
+
+
+function addEvenNumbersElements(numbers) {
+  let ul = document.querySelector('ul');
+  let evenNumbers = [];
+
+  evenNumbers = numbers.filter(function(evenNumber) {
+    return (evenNumber % 2 === 0);
+  });
+  
+  console.log(evenNumbers);
+  
+  evenNumbers.forEach(function(evenNumber) {
+    let li = document.createElement('li');
+    li.textContent = evenNumber;
+    ul.appendChild(li);
+  });
+}
+
+
+
+
+
+
+
